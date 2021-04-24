de1a5e410116d1e6099fc8a1d068a58298b1957c
T L SAI GOPINADH <gopinadh546@gmail.com>
2021-04-24 18:06:39 +0530

    Updated with adding flash and making seperate routes

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const methodOverride = require('method-override');
-const House = require('./models/house');
 const ejsMate = require('ejs-mate');
-const { houseSchema, reviewSchema } = require('./schemas.js');
-const catchAsync = require('./utils/catchAsync');
+const session = require('express-session');
+const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
-const Review = require('./models/review');
+const methodOverride = require('method-override');
+
+const houses = require('./routes/houses');
+const reviews = require('./routes/reviews');
 
 
 mongoose.connect('mongodb://localhost:27017/shiftnow', {
 	useNewUrlParser: true,
 	useCreateIndex: true,
-	useUnifiedTopology: true
+	useUnifiedTopology: true,
+	useFindAndModify: false,
 });
 
 const db = mongoose.connection;
@@ -31,91 +33,39 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-const validateHouse = (req, res, next) => {
-	const { error } = houseSchema.validate(req.body);
-	if (error) {
-		const msg = error.details.map(el => el.message).join(',')
-		throw new ExpressError(msg, 400)
-	} else {
-		next();
+
+const sessionConfig = {
+	secret: 'thisshouldbeabettersecret',
+	resave: false,
+	saveUninitialized: true,
+	cookie: {
+		httpOnly: true,
+		expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+		maxAge: 1000 * 60 * 60 * 24 * 7
 	}
 }
 
-const validateReview = (req, res, next) => {
-	const { error } = reviewSchema.validate(req.body);
-	if (error) {
-		const msg = error.details.map(el => el.message).join(',')
-		throw new ExpressError(msg, 400)
-	} else {
-		next();
-	}
-}
+app.use(session(sessionConfig))
+app.use(flash());
+
+
+app.use((req,res,next) =>{
+	res.locals.success = req.flash('success');
+	res.locals.error = req.flash('error');
+	next();
+})
+
+app.use('/houses', houses);
+app.use('/houses/:id/reviews', reviews);
+
+
 
 app.get('/', (req, res) => {
 	res.render('home')
 });
 
-app.get('/houses', catchAsync(async (req, res) => {
-	const houses = await House.find({});
-	res.render('houses/index', { houses })
-}));
-
-app.get('/houses/new', (req, res) => {
-	res.render('houses/new')
-});
-
-app.post('/houses', validateHouse, catchAsync(async (req, res, next) => {
-	// if (!req.body.house) throw new ExpressError('Invalid House Data', 400);
-	const house = new House(req.body.house);
-	await house.save();
-	res.redirect(`/houses/${house._id}`)
-
-}))
-
-app.get('/houses/:id', catchAsync(async (req, res) => {
-	const house = await House.findById(req.params.id).populate('reviews');
-	let totalRating = 0;
-	let count = 0;
-	for(let rat of house.reviews){
-		totalRating = totalRating + rat.rating;
-		count = count + 1;
-	}
-	res.render('houses/show', { house, totalRating, count })
-}));
-
-app.get('/houses/:id/edit', catchAsync(async (req, res) => {
-	const house = await House.findById(req.params.id)
-	res.render('houses/edit', { house })
-}));
-
-app.put('/houses/:id', validateHouse, catchAsync(async (req, res) => {
-	const { id } = req.params;
-	const house = await House.findByIdAndUpdate(id, { ...req.body.house });
-	res.redirect(`/houses/${house._id}`)
-}));
-
-app.delete('/houses/:id', catchAsync(async (req, res) => {
-	const { id } = req.params;
-	await House.findByIdAndDelete(id);
-	res.redirect('/houses');
-}));
-
-app.post('/houses/:id/reviews', validateReview, catchAsync(async (req, res) => {
-	const house = await House.findById(req.params.id);
-	const review = new Review(req.body.review);
-	house.reviews.push(review);
-	await review.save();
-	await house.save();
-	res.redirect(`/houses/${house._id}`);
-}));
-
-app.delete('/houses/:id/reviews/:reviewId', catchAsync(async (req, res) => {
-	const { id, reviewId } = req.params;
-	await House.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-	await Review.findByIdAndDelete(reviewId);
-	res.redirect(`/houses/${id}`);
-}))
 
 app.all('*', (req, res, next) => {
 	next(new ExpressError('Page Not Found', 404))
